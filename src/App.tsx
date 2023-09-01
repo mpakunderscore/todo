423cb798fbf179758cd6c1eeae9976455cf33a94
mpakunderscore <mpakunderscore@gmail.com>
2023-09-01 15:03:14 +0400

    Fix for heroku

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './css/index.css'
+
+function App() {
+
+    const isMobile = window.innerWidth <= 600
+
+    return (
+        <div id={'app'}>TODO</div>
+    )
+}
+export default App
